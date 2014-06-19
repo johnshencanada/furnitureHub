150d4000bb481722b49c409d709123415952a9e5
john shen <johnshencanada@gmail.com>
2014-06-19 04:03:17 -0700

    First

---
+++ 家居商城/PhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.h
+//  家居商城
+//
+//  Created by john on 6/19/14.
+//  Copyright (c) 2014 Banana Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UICollectionViewController
+
+@end
---
+++ 家居商城/PhotosViewController.m
@@ -0,0 +1,104 @@
+//
+//  PhotosViewController.m
+//  家居商城
+//
+//  Created by john on 6/19/14.
+//  Copyright (c) 2014 Banana Technology. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+
+- (instancetype)init
+{
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+    layout.itemSize = CGSizeMake(320, 250);
+    layout.minimumInteritemSpacing = 1.0;
+    layout.minimumLineSpacing = 2.0;
+    self = [super initWithCollectionViewLayout:layout];
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"photo"];
+    self.collectionView.backgroundColor = [UIColor blackColor];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"photo" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor lightGrayColor];
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender
+{
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender
+{
+	
+}
+*/
+
+@end
